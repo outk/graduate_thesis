fd884059a89ea97a68d2d02cc146af99024e113c
Tetsuya <outk1023@gmail.com>
2019-11-30 20:37:52 +0900

    modify a little and add comment

--- iterative_tomography.py
+++ iterative_tomography.py
@@ -11,6 +11,7 @@ import numpy as np
 from numpy import array, kron, trace, dot, identity
 
 
+
 """ 
 definition of base: 
 
@@ -21,6 +22,7 @@ definition of base:
     bL (        "        )
 
 """
+
 bH = array([[1,0],[0,0]])
 bV = array([[0,0],[0,1]])
 bD = array([[1/2,1/2],[1/2,1/2]])
@@ -34,15 +36,13 @@ matrix of bases for two qubits:
     
     bases (numpy.array ((4*4)*16)) 
 
+
+------------------
+
+A order of this bases array is one of most important things in calcuration.
+So you must match each other between this and data set.
+
 """
-# bases = array(
-#     [
-#     kron(bH,bH),kron(bH,bV),kron(bV,bH),kron(bV,bV),
-#     kron(bH,bD),kron(bH,bL),kron(bD,bH),kron(bR,bH),
-#     kron(bD,bD),kron(bR,bD),kron(bR,bL),kron(bD,bR),
-#     kron(bD,bV),kron(bR,bV),kron(bV,bD),kron(bV,bL)
-#     ]
-#     )
 
 bases =array(
     [
@@ -52,6 +52,7 @@ bases =array(
     kron(bV,bD),kron(bV,bL),kron(bH,bL),kron(bR,bL)
     ]
     )
+
 
 
 """
@@ -77,49 +78,44 @@ def getDatasFromFile(fileOfExperimentalDatas, numberOfQubits):
 Iterative algorithm 
 
 """
-def doIterativeAlgorithm(matrix, maxNumberOfIteration, listAsExperimentalDatas):
+
+def doIterativeAlgorithm(maxNumberOfIteration, listOfExperimentalDatas):
     """
     doIterativeAlgorithm():
 
-        This function is to do iterative algorithm(10.1103/PhysRevA.63.040303) to given matrix,
-        and return modified matrix (np.array) as outcome.
+        This function is to do iterative algorithm(10.1103/PhysRevA.63.040303) to a set of datas given from a experiment.
+        This recieve two variables (maxNumberOfIteration, listAsExperimentalDatas),
+        and return most likely estimated density matrix (np.array).
+
+        First quantum state matrix for this algorithm is a identity matrix.
 
     """
 
-    i = 0
+    iter = 0
     dimH = 4
-    m = 0
-    n = 1
-    s = 1
     epsilon = 10000000
     TolFun = 10e-10
-    dimH = 4
-    m = 0
-
-    dataList = listAsExperimentalDatas
-    totalCountOfData = sum(dataList)
-    nDataList = dataList/totalCountOfData # nDataList is a list of normarized data
-
-    # matrix = np.identity(4) # Input Density Matrix in Diluted MLE  (Identity)
     diff = 100
     traceDistance = 100
 
-    modifiedMatrix = matrix.copy()
-    if modifiedMatrix is matrix:
-        print("Matrix failed to be copyed deeply for doing iterative algorithm.")
-        return -1
+    dataList = listOfExperimentalDatas
+    totalCountOfData = sum(dataList)
+    nDataList = dataList / totalCountOfData # nDataList is a list of normarized datas
 
-    while traceDistance > TolFun and i <= maxNumberOfIteration:
+    matrix = identity(dimH) # Input Density Matrix in Diluted MLE  (Identity)
+
+
+    while traceDistance > TolFun and iter <= maxNumberOfIteration:
 
         probList = [trace(dot(matrix,bases[i])) for i in range(16)]
-        nProbList = probList/sum(probList)
+        nProbList = probList / sum(probList)
         # TODO: Why is probList used not nProbList here?
-        rotationMatrix = sum([(nDataList[i]/probList[i])*bases[i] for i in range(16)])
-        # rotationMatrix = sum([(nDataList[i]/nProbList[i])*bases[i] for i in range(16)])
+        rotationMatrix = sum([(nDataList[i] / probList[i])*bases[i] for i in range(16)])
+        # rotationMatrix = sum([(nDataList[i] / nProbList[i])*bases[i] for i in range(16)])
 
 
         """ Normalization of Matrices for Measurement Bases """
-        U = np.linalg.inv(sum(bases))/sum(probList)
+        U = np.linalg.inv(sum(bases)) / sum(probList)
         deltaRotationMatrixLeft = (identity(dimH) + epsilon*dot(U,rotationMatrix)) / (1 + epsilon)
         deltaRotationMatrixRight = (identity(dimH) + epsilon*dot(rotationMatrix,U)) / (1 + epsilon)
 
@@ -134,16 +130,17 @@ def doIterativeAlgorithm(matrix, maxNumberOfIteration, listAsExperimentalDatas):
         LikelihoodFunction = sum([nDataList[i]*np.log(nProbList[i]) for i in range(16)])
         # probList = [trace(dot(modifiedMatrix,bases[i])) for i in range(16)]
         probList = [trace(dot(bases[i],modifiedMatrix)) for i in range(16)]
-        nProbList = probList/sum(probList)
+        nProbList = probList / sum(probList)
         modifiedLikelihoodFunction = sum([nDataList[i]*np.log(nProbList[i]) for i in range(16)])
         diff = np.real(modifiedLikelihoodFunction - LikelihoodFunction)
+
 
         matrix = modifiedMatrix.copy()
         if modifiedMatrix is matrix:
             print("Matrix failed to be copyed deeply for doing iterative algorithm.")
             return -1
         
-        i += 1
+        iter += 1
 
     return modifiedMatrix
 
@@ -155,11 +152,9 @@ if __name__ == "__main__":
     # listOfExperimentalDatas = array([101599, 17900, 92913, 3263, 52733, 56453, 50266, 57325, 52234, 18951, 49106, 59811, 54621, 47110, 62711, 18847])
 
     listOfExperimentalDatas = array(list(map(float, input().split())))
-    
-    matrix = np.identity(4)
 
     maxNumberOfIteration = 10000
 
-    finalMatrix = doIterativeAlgorithm(matrix, maxNumberOfIteration, listOfExperimentalDatas)
+    finalMatrix = doIterativeAlgorithm(maxNumberOfIteration, listOfExperimentalDatas)
 
     print(finalMatrix)
